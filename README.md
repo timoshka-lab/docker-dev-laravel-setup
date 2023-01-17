8753eb4d5e0c0782fa31d7f882015c507743f00e
Timchenko Daniil <dany@timoshka-lab.co.jp>
2023-01-17 15:47:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Introduction
+このレポジトリーは社内専用となります。取引先や外部関係者のために公開しています。  
+個別のお問い合わせやご要望にはお応えできかねますことをご了承下さい。
+
+# Installation
+## 自動インストール
+```bash
+cd [WORKING_DIR]
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/timoshka-lab/docker-dev-laravel-setup/main/setup.sh)"
+```
+
+# Usage
+[https://github.com/timoshka-lab/docker-dev-laravel](https://github.com/timoshka-lab/docker-dev-laravel)
+
+# Notices
+このプロジェクトは本番環境を想定していません。  
+本番環境やステージング環境での利用は避けて下さい。  
+目的外の利用による損害については一切の責任を負いません。
+
+THIS PROJECT IS NOT INTENDED FOR PRODUCTION USE.  
+DO NOT USE IT IN PRODUCTION OR STAGING ENVIRONMENT.  
+IT IS ONLY FOR LOCAL DEVELOPMENT PURPOSES.
---
+++ setup.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function load_env() {
+  if [ -f .env ]; then
+    # shellcheck disable=SC2046
+    export $(grep -v '^#' .env | xargs)
+  fi
+}
+
+function report_error() {
+    printf "\033[0;31mERROR: %s\033[0m\n" "$1" >&2
+}
+
+function report_success_message() {
+    printf "\033[0;32m%s\033[0m\n" "$1"
+}
+
+function validate_env() {
+  required=(PHP_VERSION APP_URL APP_EMAIL MYSQL_VERSION MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD MYSQL_DATABASE NGINX_VERSION NGINX_SERVER_NAME)
+
+  for var in "${required[@]}"; do
+    if [ -z "${!var}" ]; then
+      report_error "$var is required environment variable"
+      return 1
+    fi
+  done
+
+  if ! [[ $PHP_VERSION =~ ^([0-9]+)(\.[0-9]+){0,2}$ ]]; then
+    report_error "PHP_VERSION is not set or is invalid. Valid formats: '8', '8.0', '8.0.0'"
+    return 1
+  fi
+
+  if ! [[ $APP_URL =~ ^https?://[-[:alnum:]:@/_.]*$ ]]; then
+    report_error "APP_URL is not set or is invalid. Valid formats: 'http://example.test', 'https://example.test'"
+    return 1
+  fi
+
+  if ! [[ $APP_EMAIL =~ ^[-_.+[:alnum:]]+@[-_.+[:alnum:]]+\.[[:alpha:]]+$ ]]; then
+    report_error "APP_EMAIL is not set or is invalid. Valid formats: 'wp@example.test'"
+    return 1
+  fi
+
+  if ! [[ $MYSQL_VERSION =~ ^([0-9]+)(\.[0-9]+){0,2}$ ]]; then
+    report_error "MYSQL_VERSION is not set or is invalid. Valid formats: '8', '8.0', '8.0.0'"
+    return 1
+  fi
+
+  if ! [[ $NGINX_VERSION =~ ^([0-9]+)(\.[0-9]+){0,2}$ ]]; then
+    report_error "NGINX_VERSION is not set or is invalid. Valid formats: '1', '1.0', '1.0.0'"
+    return 1
+  fi
+
+  if ! [[ $NGINX_SERVER_NAME =~ ^[-[:alnum:]/_.]*$ ]]; then
+    report_error "NGINX_SERVER_NAME is not set or is invalid. Valid formats: 'example.test'"
+    return 1
+  fi
+
+  return 0
+}
+
+function validate_env_input() {
+  read -p "Edit the '.env' file, and press enter key to continue:"
+  load_env
+
+  if ! validate_env; then
+    validate_env_input
+  fi
+}
+
+function validate_working_dir() {
+  if [ "$(ls -A | wc -l)" -gt 1 ]; then
+     echo "Error: working directory is not empty." >&2
+     exit 1
+  fi
+}
+
+function validate_dependencies() {
+  if [ ! -x "$(command -v docker)" ]; then
+    echo "Error: docker is not installed." >&2
+    exit 1
+  fi
+
+  if [ ! -x "$(command -v curl)" ]; then
+    echo "Error: curl is not installed." >&2
+    exit 1
+  fi
+}
+
+function init_project() {
+  curl -L https://github.com/timoshka-lab/docker-dev-laravel/archive/main.tar.gz | tar xvz -C ./ --strip-components=1
+  cp .env.example .env
+}
+
+function provision_docker_environment() {
+  docker compose build
+  docker compose up -d
+  docker compose exec app /setup.sh
+}
+
+function main() {
+  validate_working_dir
+  validate_dependencies
+
+  echo "Starting auto setup..."
+
+  init_project
+  validate_env_input
+  provision_docker_environment
+
+  if [ "$NGINX_ENABLE_SSL" = true ]; then
+    if [ -x "$(command -v security)" ]; then
+      echo "Installing ssl certificate into keychain..."
+      sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$(pwd)/docker/nginx/certs/server.crt"
+    else
+      echo "Warning: you have to add ssl certificate to your keychain manually."
+    fi
+  fi
+
+  report_success_message "Auto setup is now Done!"
+}
+
+main
